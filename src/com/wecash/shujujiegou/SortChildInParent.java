7d643e53dd73a09dda45bd9c4684b216b7178d9f
tong.cheng <tong.cheng@qunar.com>
2018-08-30 20:32:25 +0800

    测试

---
+++ src/com/wecash/shujujiegou/SortChildInParent.java
@@ -0,0 +1,22 @@
+package com.wecash.shujujiegou;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * User: tong.cheng
+ * Date: 2018-08-30
+ * Time: 18:09
+ */
+public class SortChildInParent {
+
+    public static void main(String[] args) {
+
+    }
+    public static List sort(List<String> childList,List<String> parentList){
+       return Lists.newArrayList();
+    }
+}
